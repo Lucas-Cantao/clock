78e20d088bda99f48679cde0746aad2fca8bf4c4
Lucas Cantão <124742219+Lucas-Cantao@users.noreply.github.com>
2023-05-07 22:35:44 -0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,25 +1,25 @@
-let hour = document.querySelector(".hour-number")
-let minute = document.querySelector(".minute-number")
-let second = document.querySelector(".second-number")
-let body = document.querySelector("#body")
-
-
-function horas(){
+ function horario(){
+    let hora = document.querySelector(".hora")
+    let minuto = document.querySelector(".minuto")
+    let segundo = document.querySelector(".segundo")
     let hor = new Date().getHours()
     let min = new Date().getMinutes()
-    let seg = new Date().getSeconds()
+    let sec = new Date().getSeconds() 
+
     if(hor<10){
         hor = `0${hor}`
     }
     if(min<10){
         min = `0${min}`
     }
-    if(seg<10){
-        seg = `0${seg}`
+    if(sec<10){
+        sec = `0${sec}`
     }
-    hour.innerHTML = hor
-    minute.innerHTML = min
-    second.innerHTML = seg
+
+    hora.innerHTML = hor
+    minuto.innerHTML = min
+    segundo.innerHTML = sec
+
 }
 
-setInterval(horas, 1000)
+setInterval(horario, 1000)
